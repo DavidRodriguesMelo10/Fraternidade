1b00e225e2539b962a7d3ed013817ffd64e53cb4
Guilherme <gui.farias@outlook.com.br>
2019-10-22 14:01:07 -0300

    começo das altereações na dashboard

--- src/components/dashboard-components/index.js
+++ src/components/dashboard-components/index.js
@@ -1,9 +1,4 @@
-import SalesSummary from './sales-summary/sales-summary';
 import Projects from './projects/projects';
 import Feeds from './feeds/feeds';
 
-export {
-    SalesSummary,
-    Projects,
-    Feeds
-};
+export { Projects, Feeds };
